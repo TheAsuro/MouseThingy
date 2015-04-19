7f35212216947a7f6cdcba1a5d0eea8c7b00ca61
TheAsuro <theasuro@gmail.com>
2015-04-19 05:45:45 +0200

    Yeaah

---
+++ MouseThingy/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace MouseThingy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bnUpdate = new System.Windows.Forms.Button();
+            this.processList = new System.Windows.Forms.ComboBox();
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbData = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataList = new System.Windows.Forms.ComboBox();
+            this.bnGo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bnUpdate
+            // 
+            this.bnUpdate.Location = new System.Drawing.Point(12, 12);
+            this.bnUpdate.Name = "bnUpdate";
+            this.bnUpdate.Size = new System.Drawing.Size(145, 23);
+            this.bnUpdate.TabIndex = 0;
+            this.bnUpdate.Text = "Update Processes";
+            this.bnUpdate.UseVisualStyleBackColor = true;
+            this.bnUpdate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // processList
+            // 
+            this.processList.FormattingEnabled = true;
+            this.processList.Location = new System.Drawing.Point(163, 12);
+            this.processList.Name = "processList";
+            this.processList.Size = new System.Drawing.Size(301, 21);
+            this.processList.TabIndex = 1;
+            this.processList.SelectionChangeCommitted += new System.EventHandler(this.processList_SelectionChangeCommitted);
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Location = new System.Drawing.Point(163, 39);
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(301, 20);
+            this.tbAddress.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Write to address:";
+            // 
+            // tbData
+            // 
+            this.tbData.Location = new System.Drawing.Point(163, 92);
+            this.tbData.Name = "tbData";
+            this.tbData.Size = new System.Drawing.Size(301, 20);
+            this.tbData.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Data type:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Data:";
+            // 
+            // dataList
+            // 
+            this.dataList.FormattingEnabled = true;
+            this.dataList.Items.AddRange(new object[] {
+            "Int",
+            "String",
+            "Float"});
+            this.dataList.Location = new System.Drawing.Point(163, 65);
+            this.dataList.Name = "dataList";
+            this.dataList.Size = new System.Drawing.Size(301, 21);
+            this.dataList.TabIndex = 8;
+            // 
+            // bnGo
+            // 
+            this.bnGo.Location = new System.Drawing.Point(12, 118);
+            this.bnGo.Name = "bnGo";
+            this.bnGo.Size = new System.Drawing.Size(452, 23);
+            this.bnGo.TabIndex = 9;
+            this.bnGo.Text = "GO";
+            this.bnGo.UseVisualStyleBackColor = true;
+            this.bnGo.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 148);
+            this.Controls.Add(this.bnGo);
+            this.Controls.Add(this.dataList);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbData);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbAddress);
+            this.Controls.Add(this.processList);
+            this.Controls.Add(this.bnUpdate);
+            this.Name = "Form1";
+            this.Text = "Test Thingy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bnUpdate;
+        private System.Windows.Forms.ComboBox processList;
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbData;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox dataList;
+        private System.Windows.Forms.Button bnGo;
+    }
+}
+
---
+++ MouseThingy/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace MouseThingy
+{
+    public partial class Form1 : Form
+    {
+        const int PROCESS_VM_WRITE = 0x0020;
+        const int PROCESS_VM_OPERATION = 0x0008;
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll")]
+        private static extern Boolean WriteProcessMemory(IntPtr hProcess, uint lpBaseAddress, byte[] lpBuffer, int nSize, IntPtr lpNumberOfBytesWritten);
+
+        private Process proc;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            processList.Items.Clear();
+
+            Process[] processes = Process.GetProcesses();
+            foreach(Process process in processes)
+            {
+                processList.Items.Add(process.ProcessName);
+            }
+        }
+
+        private void button2_Click(object sender, System.EventArgs e)
+        {
+            byte[] data = null;
+
+            switch (dataList.SelectedItem.ToString())
+            {
+                case "Int":
+                    int unconvertedInt;
+                    if (int.TryParse(tbData.Text, out unconvertedInt))
+                    {
+                        data = BitConverter.GetBytes(unconvertedInt);
+                    }
+                    break;
+                case "String":
+                    char[] unconvertedString = tbData.Text.ToCharArray();
+                    data = Encoding.UTF8.GetBytes(unconvertedString);
+                    break;
+                case "Float":
+                    float unconvertedFloat;
+                    if (float.TryParse(tbData.Text, out unconvertedFloat))
+                    {
+                        data = BitConverter.GetBytes(unconvertedFloat);
+                    }
+                    break;
+                default:
+                    MessageBox.Show("Please select a valid type!");
+                    return;
+            }
+
+            if (data == null)
+            {
+                MessageBox.Show("Couldn't parse data!");
+                return;
+            }
+
+            uint memAddr;
+            if (!uint.TryParse(tbAddress.Text, out memAddr))
+            {
+                MessageBox.Show("Couldn't parse address!");
+                return;
+            }
+
+            int bytesWritten = 0;
+            IntPtr procHandle = OpenProcess(PROCESS_VM_WRITE | PROCESS_VM_OPERATION, false, proc.Id);
+            if (WriteProcessMemory(procHandle, memAddr, data, data.Length, new IntPtr(bytesWritten)))
+            {
+                MessageBox.Show("Success!\nGot " + data.Length + " bytes, \n" + "Wrote " + bytesWritten + " bytes!");
+            }
+            else
+            {
+                MessageBox.Show("Failed!\nGot " + data.Length + " bytes, \n" + "Wrote " + bytesWritten + " bytes!");
+            }            
+        }
+
+        private void processList_SelectionChangeCommitted(object sender, System.EventArgs e)
+        {
+            Process[] procs = Process.GetProcessesByName(processList.SelectedItem.ToString());
+            proc = procs[0];
+        }
+    }
+}
